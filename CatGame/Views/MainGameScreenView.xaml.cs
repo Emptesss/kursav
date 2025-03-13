b797192f5fbb825a4f1597ff0007c9564cbd4c01
Emptesss <katyakirilchik2007@mail.ru>
2025-03-13 21:06:29 +0300

    peniseek

--- CatGame/Views/MainGameScreenView.xaml.cs
+++ CatGame/Views/MainGameScreenView.xaml.cs
@@ -24,5 +24,10 @@ namespace CatGame.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
